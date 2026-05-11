3ef81fa2fe1073c6845d2630eeb1ec802aa7cae9
Eduarda Nakano <eduarda.nakano.torres@escola.pr.gov.br>
2026-05-11 12:03:01 -0300

    terminei o 1

--- Exercício 1.js
+++
@@ -1,6 +0,0 @@
-//1. 
-let nome = input.question("");
-let idade = input.question("");
-let cidade = input.question("");
-
-'Ola! Meu nome e ${nome}, tenho ${idade} e sou de ${cidade}';
---
+++ ex1.js
@@ -0,0 +1,7 @@
+//1. 
+const input = require('readline-sync');
+let nome = input.question("");
+let idade = input.question("");
+let cidade = input.question("");
+
+console.log ("Ola! Meu é ", nome, ", tenho ", idade, " anos e sou de ", cidade, ".")
