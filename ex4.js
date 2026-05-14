9d5587252fbd1601df5f2032549023a292450b30
dnakano10 <eduardaayuminakano@gmail.com>
2026-05-14 10:46:29 -0300

    exercicios

--- ex4.js
+++ ex4.js
@@ -1,3 +1,8 @@
 //4.
 const input = require('readline-sync');
 
+let tab = input.questionInt("Digite a tabuada que deseja: ");
+
+for(let i=1; i<=10; i++) {
+    console.log(tab + " x " + i + " = " + (tab*i));
+}
